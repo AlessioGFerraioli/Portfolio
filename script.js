593036ce6cf5c80f0d00e048b1fe38ddb74da061
Alessio Giuseppe Ferraioli <alessioferraioli@hotmail.it>
2025-02-10 14:40:34 +0100

    added featured projects and fixed styles

--- script.js
+++ script.js
@@ -98,8 +98,3 @@ function isTouchDevice() {
   return ('ontouchstart' in window) || (navigator.maxTouchPoints > 0);
 }
 
-// Existing code...
-
-// Get references to elements
-// Existing code for toggle button and custom cursor...
-
