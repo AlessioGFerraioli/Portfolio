170955b3267e074e9faf27ef8ee693c22a944113
Alessio Giuseppe Ferraioli <alessioferraioli@hotmail.it>
2025-10-29 12:58:04 +0100

    new index page

---
+++ new/script.js
@@ -0,0 +1,16 @@
+/*
+let emptyElement=document.querySelector(".about .empty"),
+titleElement=document.querySelector(".about .title"),
+figureElements=document.querySelectorAll(".service figure");
+
+function handleMouseMove(e) {
+    emptyElement.style.flexBasis = e.clientX+"px",
+    titleElement.style.flexBasis = e.clientY/2+"px",
+    figureElements.forEach( function (t) {
+        t.style.flexBasis=window.innerWidth-e.clientX+"px"}
+    )}
+
+//console.log(figureElements),
+window.addEventListener("mousemove",handleMouseMove);
+
+*/
